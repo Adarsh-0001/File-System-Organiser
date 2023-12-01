a4b8c79611a3b0df5b5ad574b1c9a2104963ec93
adarsh srivastava <adarshadisrivastava1996@gmail.com>
2023-12-02 02:23:41 +0530

    creating help function

---
+++ help.js
@@ -0,0 +1,12 @@
+function helpFunc(){
+    console.log(`
+    List of all commands:
+        node main.js tree "directoryPath"
+        node main.js organise "directoryPath"
+        node main.js help
+    `)
+}
+module.exports={
+    helpKey: helpFunc
+}
+console.log(helpFunc());
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-let inputArray = process.argv.slice(2);
+// let inputArray = process.argv.slice(2);
 let fileSys = require("fs");
 let filePath = require("path");
-
-
+let helpFile = require("./help");
+// let treeFile = require("tree");
+// let orgFile = require("organise");
 // console.log(inputArray);
+console.log(helpFile);
